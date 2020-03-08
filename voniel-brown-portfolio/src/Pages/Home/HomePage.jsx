c3387baa942c709e63005587acb267bc77d710d6
vonbarown <vonielbrown@pursuit.org>
2020-03-08 10:49:12 -0400

    adding box sizing to all elements

--- voniel-brown-portfolio/src/Pages/Home/HomePage.jsx
+++ voniel-brown-portfolio/src/Pages/Home/HomePage.jsx
@@ -13,12 +13,14 @@ const Home = () => {
     const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
     return (
         <div className='homePage'>
-            <div className="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-                <animated.div className="card1" style={{ transform: props.xy.interpolate(trans1) }} />
-                <animated.div className="card2" style={{ transform: props.xy.interpolate(trans2) }} />
-                <animated.div className="card3" style={{ transform: props.xy.interpolate(trans3) }} />
-                <animated.div className="card4" style={{ transform: props.xy.interpolate(trans4) }} />
-            </div>
+            {
+                // <div className="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+                //     <animated.div className="card1" style={{ transform: props.xy.interpolate(trans1) }} />
+                //     <animated.div className="card2" style={{ transform: props.xy.interpolate(trans2) }} />
+                //     <animated.div className="card3" style={{ transform: props.xy.interpolate(trans3) }} />
+                //     <animated.div className="card4" style={{ transform: props.xy.interpolate(trans4) }} />
+                // </div>
+            }
         </div>
     )
 }
