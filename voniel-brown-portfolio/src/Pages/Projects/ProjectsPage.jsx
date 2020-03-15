0d2d5d82e32dd4c04465274c4880f34f1b2b5dac
vonbarown <vonielbrown@pursuit.org>
2020-03-15 13:51:36 -0400

    sidescrolling for projects on mobile

--- voniel-brown-portfolio/src/Pages/Projects/ProjectsPage.jsx
+++ voniel-brown-portfolio/src/Pages/Projects/ProjectsPage.jsx
@@ -1,69 +1,66 @@
-import React from 'react'
-import './portfolio.css'
-import { ProjectCard } from './projectCard'
+import React from "react";
+import "./portfolio.css";
+import { ProjectCard } from "./projectCard";
 
 const Portfolio = () => {
-
-    const projects = [
-        {
-            title: `Synergie`,
-            description: `Synergie is a full-stack react app, that allows users to keep track of their favorite shows.
+  const projects = [
+    {
+      title: `Synergie`,
+      description: `Synergie is a full-stack react app, that allows users to keep track of their favorite shows.
                         Allowing users to plan their binging sessions with their friends.We know that life can be busy, now you can keep track of the
                         shows that you love.`,
-            img_url: ``,
-            live: '',
-            repo: ''
-        },
-        {
-            title: `fwrfgw`,
-            description: `just`,
-            img_url: ``,
-            live: '',
-            repo: ''
-        },
-        {
-            title: `fwrdfqd`,
-            description: `a`,
-            img_url: ``,
-            live: '',
-            repo: ''
-        },
-        // {
-        //     title: `fwrdgq`,
-        //     description: `mellow`,
-        //     img_url: ``,
-        //     live: '',
-        //     repo: ''
-        // },
-        // {
-        //     title: `fwrdgqdefq`,
-        //     description: `fellow`,
-        //     img_url: ``,
-        //     live: '',
-        //     repo: ''
-        // },
-        // {
-        //     title: `fwrdgdqqdefq`,
-        //     description: `just`,
-        //     img_url: ``,
-        //     live: '',
-        //     repo: ''
-        // },
+      img_url: ``,
+      live: "",
+      repo: ""
+    },
+    {
+      title: `fwrfgw`,
+      description: `just`,
+      img_url: ``,
+      live: "",
+      repo: ""
+    },
+    {
+      title: `fwrdfqd`,
+      description: `a`,
+      img_url: ``,
+      live: "",
+      repo: ""
+    },
+    {
+      title: `fwrdgq`,
+      description: `mellow`,
+      img_url: ``,
+      live: "",
+      repo: ""
+    },
+    {
+      title: `fwrdgqdefq`,
+      description: `fellow`,
+      img_url: ``,
+      live: "",
+      repo: ""
+    },
+    {
+      title: `fwrdgdqqdefq`,
+      description: `just`,
+      img_url: ``,
+      live: "",
+      repo: ""
+    }
+  ];
 
-    ]
-
-    return (
-        <div className='portfolioPage'>
-            <div className='inner-frame'>
-                <div className='flex'>
-                    {
-                        projects.map(project => <ProjectCard project={project} key={project.title} />)
-                    }
-                </div>
-            </div>
+  return (
+    <div className="portfolioPage">
+      <div className="inner-frame">
+        <div className="flex">
+          {projects.map(project => (
+            <ProjectCard project={project} key={project.title} />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-
-export default Portfolio
+export default Portfolio;
--- voniel-brown-portfolio/src/Pages/Projects/projectCard.jsx
+++ voniel-brown-portfolio/src/Pages/Projects/projectCard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const ProjectCard = ({ project: { title, description, img_url } }) => {
-    return (
-        <div className='project-card' key={title}>
-            <p>{title}</p>
-            <p>{description}</p>
-            <img src={img_url} alt={title} />
-        </div>
-    )
-}
+  return (
+    <div className="project-card" key={title} id="project-card">
+      <p>{title}</p>
+      <p>{description}</p>
+      <img src={img_url} alt={title} />
+    </div>
+  );
+};
