ea7629c4e9db9502495d7db78fcc53630703072c
vonbarown <vonielbrown@pursuit.org>
2020-03-15 14:42:21 -0400

    adding in the skills component

---
+++ voniel-brown-portfolio/src/Components/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Skills = () => {
+  return (
+    <div>
+      hola
+      <p>come</p>
+    </div>
+  );
+};
--- voniel-brown-portfolio/src/Pages/Contact/Contact.jsx
+++ voniel-brown-portfolio/src/Pages/Contact/Contact.jsx
@@ -1,86 +1,26 @@
-import React, { Component } from 'react'
-// import axios from 'axios'
+import React from "react";
+import { Skills } from "../../Components/Skills/Skills";
+import "./contact.css";
 
-import './contact.css'
-class Contact extends Component {
-    constructor(props) {
-        super(props)
+const Contact = () => {
+  const handleButtonClick = () =>
+    (window.location.href = `mailto:vonielbrown@pursuit.org`);
 
-        this.state = {
-            name: '',
-            email: '',
-            message: ''
-        }
-    }
+  return (
+    <div className="contactPage">
+      <div className="contactPage-formField">
+        <div className="skills-component-contact">
+          <Skills />
+        </div>
 
-    handleInput = e => this.setState({ [e.target.name]: e.target.value });
+        <div className="contact-btn-container">
+          <button className="submit button-theme" onClick={handleButtonClick}>
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-    // handleSubmit = async e => {
-    //     e.preventDefault()
-    //     const { name, email, message } = this.state
-
-    //     try {
-    //         axios.post('http://localhost:8080/users/send',
-    //             {
-    //                 name,
-    //                 email,
-    //                 message
-    //             }
-    //         )
-
-    //         // console.log(mail);
-
-    //     } catch (error) {
-    //         console.log(error);
-
-    //     }
-    // }
-
-    handleButtonClick = () => window.location.href = `mailto:vonielbrown@pursuit.org?subject=New%20Message%20from%20${this.state.name}%20on%20Portfolio%20Website%20&body=${this.state.message}`
-
-    render() {
-        console.log(this.state);
-
-        return (
-            <div className='contactPage'>
-                <div className='contactPage-formField'>
-                    <form className='contact' onSubmit={e => e.preventDefault()} required>
-                        <div className='inputField'>
-                            <input
-                                name='name'
-                                type="text"
-                                placeholder=' Name..'
-                                onChange={this.handleInput}
-                                value={this.state.name}
-                                required
-                                className='inputBar contact-input'
-                            />
-                        </div>
-                        <div className='inputField'>
-                            <textarea
-                                className='contact-input text-area'
-                                name="message"
-                                id="message"
-                                cols="30"
-                                rows="10"
-                                placeholder='Message....'
-                                onChange={this.handleInput}
-                                value={this.state.message}
-                                required
-                            >
-                            </textarea>
-                        </div>
-                        <button
-                            className='submit button-theme'
-                            onClick={this.handleButtonClick}
-                        >Submit
-                        </button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Contact
+export default Contact;
