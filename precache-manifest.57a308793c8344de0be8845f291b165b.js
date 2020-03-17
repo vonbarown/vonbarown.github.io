4c1478f28cb8cceb46ea8c8b510f8ed67ee6e19a
vonbarown <vonielbrown@pursuit.org>
2020-03-16 20:47:51 -0400

    Updates

---
+++ precache-manifest.57a308793c8344de0be8845f291b165b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1d9c28ef40debe9c6cd827896ef4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b658398df63e4e6164ff",
+    "url": "/static/css/main.3cc70273.chunk.css"
+  },
+  {
+    "revision": "ee646d060804966dd903",
+    "url": "/static/js/2.cad2b360.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cad2b360.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b658398df63e4e6164ff",
+    "url": "/static/js/main.6c8644c2.chunk.js"
+  },
+  {
+    "revision": "73ce2a83a1b64c36e639",
+    "url": "/static/js/runtime-main.452672ed.js"
+  },
+  {
+    "revision": "d2120959a393e3b8aa2b84f597aa327b",
+    "url": "/static/media/synergie-landing.d2120959.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.92d2b4bd3d20ab2b50fdd7919f788bc8.js"
+  "/precache-manifest.57a308793c8344de0be8845f291b165b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
