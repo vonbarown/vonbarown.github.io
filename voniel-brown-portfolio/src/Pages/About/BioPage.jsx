49d1134f7bc17d16457c6081491fa28770e85e79
vonbarown <vonielbrown@pursuit.org>
2020-03-17 21:35:25 -0400

    installing font-family into app

--- voniel-brown-portfolio/src/Pages/About/BioPage.jsx
+++ voniel-brown-portfolio/src/Pages/About/BioPage.jsx
@@ -20,22 +20,12 @@ const About = () => {
             I came to the United States from Jamaica when I was 8 years old in
             order get a better education. I was my very first time ever leaving
             the country, When I first arrived it was a drastic change in culture
-            and environment.
+            and environment. It was an overwhelming experience, however it has
+            taught me how to adapt to a diverse and changing environment.
           </p>
           <p className="para-2">
             I was immediately drawn in by all the experiences around me
           </p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa id
-          neque aliquam vestibulum morbi blandit cursus risus. Enim facilisis
-          gravida neque convallis a cras. Tincidunt nunc pulvinar sapien et.
-          Tempus urna et pharetra pharetra massa. Ac turpis egestas sed tempus
-          urna et. Id venenatis a condimentum vitae sapien pellentesque habitant
-          morbi. Augue ut lectus arcu bibendum at. Tellus orci ac auctor augue
-          mauris. Iaculis eu non diam phasellus vestibulum lorem. Volutpat est
-          velit egestas dui id ornare arcu odio. A lacus vestibulum sed arcu non
-          odio euismod lacinia at. Ac tortor vitae purus faucibus ornare
-          suspendisse sed nisi lacus. Velit laoreet id donec ultrices tincidunt.
         </div>
       </div>
     </div>
