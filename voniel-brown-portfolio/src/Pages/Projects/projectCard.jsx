9a4057d26aee90af753632f14cb2d9c0e9b04194
vonbarown <vonielbrown@pursuit.org>
2020-03-20 22:01:03 -0400

    cleaning up package json and finding a new card style

--- voniel-brown-portfolio/src/Pages/Projects/projectCard.jsx
+++ voniel-brown-portfolio/src/Pages/Projects/projectCard.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import "./projects.css";
+// import "./projects.css";
+import "./test.scss";
 
 export const ProjectCard = ({ project: { title, description, img_url } }) => {
   return (
     <div className="project-card" key={title} id="project-card">
-      <div className="project-img-container">
-        <img className="project-img" src={img_url} alt={title} />
-      </div>
-      <div className="project-info">
-        <p>{title}</p>
-        <p>{description}</p>
+      <div className="wrapper">
+        <div className="card">
+          <input
+            type="checkbox"
+            id={title}
+            className="more"
+            aria-hidden="true"
+          />
+          <div className="content">
+            <div
+              className="front"
+              style={{
+                backgroundImage: `url(${img_url})`
+              }}
+            >
+              <div className="inner">
+                <h2>{title}</h2>
+                <label for={title} className="button" aria-hidden="true">
+                  Details
+                </label>
+              </div>
+            </div>
+            <div className="back">
+              <div className="inner">
+                <div className="description">
+                  <p>{description}</p>
+                </div>
+                <label for={title} className="button return" aria-hidden="true">
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
