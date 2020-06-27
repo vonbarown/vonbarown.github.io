7933e3b71b5c303511720a449b22dd6cafb3b9de
vonbarown <vonielbrown@pursuit.org>
2020-06-26 21:22:41 -0400

    adding in skills table

--- src/index.js
+++ src/index.js
@@ -7,4 +7,16 @@ render(<App />, document.getElementById("root"));
 
 let infoMsg = `Hello there welcome to my personal portfolio`;
 
+const frontEndSkills = [
+  "Javascript/ES6",
+  "Redux",
+  "React",
+  "React Native",
+  "Typescript",
+  "HTML5/CSS3",
+];
+
+const backEndSkills = ["Node.js/Express", "PostgreSQL"];
+
 console.info(infoMsg);
+console.table({ frontend: frontEndSkills, backend: backEndSkills });
