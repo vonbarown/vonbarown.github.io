fabb0fe20506b54ac93412e42439a4eb484a30a2
vonbarown <vonielbrown@pursuit.org>
2020-07-19 10:28:46 -0400

    adding in error boundaries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import React from "react";
-import App from "./App";
+import App from "./Portfolio";
 import { render } from "react-snapshot";
 dotenv.config();
 render(<App />, document.getElementById("root"));
