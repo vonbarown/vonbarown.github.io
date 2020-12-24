da8c7ad59a0c42086c8ce7cc403456e9450ce5ba
Voniel B. Brown <52142269+vonbarown@users.noreply.github.com>
2020-12-24 13:13:42 -0500

    Update ProjectsPage.jsx

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import "./projects.css";
 import { ProjectCard } from "./Card";
-import synergie_landing from "../../Assets/synergie-landing.png";
-import portfolio_light from "../../Assets/portfolio_light.png";
-import portfolio_page from "../../Assets/portfolio_page.png";
-import A9 from "../../Assets/A9.png";
-import SDM from "../../Assets/student-data-management.png";
-import pantry from "../../Assets/pantry.png";
 import { projects } from './constants'
 
 const Projects = ({ darkMode }) => {
