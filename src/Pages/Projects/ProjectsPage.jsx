9d5d37a9dbaf4bedfc780faf0d99ae4faad80f42
Voniel B. Brown <52142269+vonbarown@users.noreply.github.com>
2020-12-24 13:40:51 -0500

    Update ProjectsPage.jsx

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -4,12 +4,13 @@ import { ProjectCard } from "./Card";
 import { projects } from './constants'
 
 const Projects = ({ darkMode }) => {
+  const listOfProjects = projects(darkMode)
   return (
     <div className="portfolioPage">
       <h1 className="page-title">Projects</h1>
       <div className="inner-frame">
         <div className="flex">
-          {projects.map((project) => (
+          {listOfProjects.map((project) => (
             <ProjectCard project={project} key={project.title} />
           ))}
         </div>
